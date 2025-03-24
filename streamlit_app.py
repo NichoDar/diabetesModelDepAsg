e745e6eaf528e1d73a870da7c43418bff23ab86a
NichoDar <nichodarr@gmail.com>
2025-03-24 20:01:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Diabetes Classification')
 
-st.write('Diabetes categories obtained using Machine Learning and OOP')
+st.info('Diabetes categories obtained using Machine Learning and OOP')
